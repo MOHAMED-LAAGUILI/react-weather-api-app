72ed29fbc1c7e7e81acb15cb5dc08597a1cb2cf9
MOHAMED LAAGUILI | Junior Dev <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2024-11-14 21:23:06 +0100

    Update WeatherContext.jsx

--- src/contexts/WeatherContext.jsx
+++ src/contexts/WeatherContext.jsx
@@ -21,7 +21,7 @@ export const StateContextProvider = ({ children }) => {
     //const apiKey = process.env.VITE_X_RAPID_API_KEY;  // process is undefined
 
   
-    const apiKey =process.env.VITE_X_RAPID_API_KEY;;
+    const apiKey = process.env.VITE_X_RAPID_API_KEY || import.meta.env.VITE_X_RAPID_API_KEY ;
     const options = {
       method: 'GET',
       url: 'https://visual-crossing-weather.p.rapidapi.com/forecast',
